d98c922d4d1b87102425e42d643dc48358819e0a
IsraelCastroGonzalez <israel.casgon@gmail.com>
2021-03-15 13:45:07 -0600

    Autocomplete from suggestions

--- js/main.js
+++ js/main.js
@@ -1,30 +1,61 @@
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
+const submitBtn = document.getElementById('submit-btn');
 
 //Request and filter
-const searchCountries = async searchText => {
+const suggestCountries = async searchText => {
     //Verify that we have more than 3 characters
     if(search.value.length > 3){
-        const res = await fetch('https://restcountries.eu/rest/v2/name/'+search.value+'?fields=name;capital;alpha3Code');
+        const res = await fetch('https://restcountries.eu/rest/v2/name/'+search.value+'?fields=name;alpha3Code');
         const countries = await res.json();        
         //console.log(countries);
-        outputHTML(countries);
+        if(res.ok){
+          outputHTML(countries);
+        }
+        else{
+          matchList.innerHTML = '';
+          let div = document.createElement("div");
+          div.setAttribute("id","no-match");
+          div.setAttribute("class", "list-item");
+          
+          let h5 = document.createElement("h5");
+          h5.innerHTML = "No match found.";
+          h5.setAttribute("class", "text-danger");
+          div.appendChild(h5);
+          matchList.append(div);
+        }
+        
     }else{
         matchList.innerHTML = '';
     }
 }
 
 const outputHTML = countries =>{
-    if(countries.length > 0){
-        const html = countries.map(country =>`
-            <div class="card card-body card-body-hover mb-1">
-                <h4>${country.name}</h4><span class="text-primary">${country.capital}</span>
-                <small>${country.alpha3Code}</small>
-            </div>
-        `).join(''); // convert to pure txt
+  matchList.innerHTML = '';
+  if(countries.length > 0){
+    countries.forEach(country => {
+      let div = document.createElement("div");
+      div.setAttribute("id","list-item");
+      div.setAttribute("class", "list-item");
+      
+      let h5 = document.createElement("h5");
+      h5.innerHTML = country.name;
+      div.appendChild(h5);
+      
+      let p = document.createElement("p");
+      p.innerHTML = country.alpha3Code;
+      p.setAttribute("class", "text-primary");
+      div.appendChild(p);
+      
+      div.innerHTML += "<input type='hidden' value='" + country.name + "'>";
 
-        matchList.innerHTML = html;
-    }
-} 
+      div.addEventListener("click", function(e) {
+        search.value = this.getElementsByTagName("input")[0].value;
+        matchList.innerHTML = '';
+      });
+      matchList.append(div);
+    });
+  }
+}
+search.addEventListener('input',()=> suggestCountries(search.value));
 
-search.addEventListener('input',()=> searchCountries(search.value));        
